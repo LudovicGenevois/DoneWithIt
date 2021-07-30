0c620a8a0f3f828c1984570c0adc54e2551fffec
Ludovic Genevois <ludovic.genevois@hotmail.fr>
2021-07-30 15:17:39 +0200

    Modification des paramètres des JSON de l'app et modification de App.js

--- App.js
+++ App.js
@@ -1,32 +1,10 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, SafeAreaView, Button, Alert, Platform, StatusBar, View, Dimensions} from 'react-native';
-import { useDimensions } from '@react-native-community/hooks/lib/useDimensions';
+import HomeNavigator from './app/navigations/HomeNavigator';
 
 export default function App() {
+  const isLogged = true;
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: "30%"
-        }}
-      >
-
-      </View>
-    </SafeAreaView>
+    <HomeNavigator></HomeNavigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  whiteText: {
-    color: 'white'
-  }
-});
