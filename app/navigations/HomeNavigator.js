d18c06484b1fa09ce5e863b250cd2df7d3db59c1
Ludovic Genevois <ludovic.genevois@hotmail.fr>
2021-07-30 15:28:12 +0200

    Correction d'un erreur de balise Screen dans HomeNavigator

--- app/navigations/HomeNavigator.js
+++ app/navigations/HomeNavigator.js
@@ -10,20 +10,10 @@ const Stack = createStackNavigator();
 function HomeNavigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                headerMode="none">
-                <Stack.Screen
-                    name="Home"
-                    component={HomeScreen}>
-                </Stack.Screen>
-                <Stack.Screen
-                    name="SignIn"
-                    component={SignInScreen}>
-                </Stack.Screen>
-                <Stack.Screen
-                    name="Logged"
-                    component={ViewImageScreen}>
-                </Stack.Screen>
+            <Stack.Navigator headerMode="none">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="SignIn" component={SignInScreen} />
+                <Stack.Screen name="Logged" component={ViewImageScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
